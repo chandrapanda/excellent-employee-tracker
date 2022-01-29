a9b18b53721f459e9e55ff5ab81ffab08ad3501c
Chandra Holt <chandra_holt@hotmail.com>
2022-01-29 09:54:29 -0800

    Started by creating new files and folders according to project. Installed required: mysql2, inquirer, console.table

---
+++ index.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const db = require('./db.connection');
+
+//Present user with options
+//View all departments - READ -SELECT * FROM
+//Add a Department
+
+//
+
+//view all roles - READ - "SELECT" * FROM
+//view all employees READ - "SELECT * FROM
+async function viewAllEmployees()
+
+async function createRole() 
+
+//SELECT the existing department out for th
+
+//add an employee
+
+//update employee -SELECT
